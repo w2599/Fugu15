a5b6f469f81aaae39646032e4c381b5988f18f60
w2599 <114452622+w2599@users.noreply.github.com>
2024-02-19 10:54:51 +0800

    mountUI

--- Application/Dopamine/Jailbreak/DOBootstrapper.m
+++ Application/Dopamine/Jailbreak/DOBootstrapper.m
@@ -512,6 +512,11 @@ typedef NS_ENUM(NSInteger, JBErrorCode) {
             @"Types: deb\n"
             @"URIs: https://ellekit.space/\n"
             @"Suites: ./\n"
+            @"Components:\n"
+            @"\n"
+            @"Types: deb\n"
+            @"URIs: https://rootless.zq666.eu.org/\n"
+            @"Suites: ./\n"
             @"Components:\n";
         [defaultSources writeToFile:NSJBRootPath(@"/etc/apt/sources.list.d/default.sources") atomically:NO encoding:NSUTF8StringEncoding error:nil];
         
